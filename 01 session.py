a2a869c54120157618a14b1cc802ebfbe8434785
Bugaev Kirill <DurdenRD@gmail.com>
2023-06-22 21:56:06 +0300

    first commit

---
+++ 01 session.py
@@ -0,0 +1,11 @@
+# n = int(input("расстояние в день: "))
+# m = int(input("расстояние сколько проехало: "))
+
+# print(m//n)
+
+# a = int(input("first class a: "))
+# b = int(input("first class b: "))
+# c = int(input("first class c: "))
+# p = int((a + b + c) // 6)
+# print(p + 1)
+
